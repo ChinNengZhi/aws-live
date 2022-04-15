3e2078f86a1c2a23e4d6af5df7f1f78354a12ab6
ChinNengZhi <93185328+ChinNengZhi@users.noreply.github.com>
2022-04-15 13:07:44 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cwhyo5rduj5d.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "fcuc1234"
+customdb = "employee"
+custombucket = "chinnengzhi-employee"
+customregion = "us-east-1"
